e972f0df399978a8dea39db66cd3cbb4f7d6b679
Ekyd9 <ekyd777@gmail.com>
2025-09-15 21:07:09 +0300

    update isBetweenHalfOpen

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -3,7 +3,10 @@ package ru.javawebinar.topjava.util;
 import java.time.LocalTime;
 
 public class TimeUtil {
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return !lt.isBefore(startTime) && lt.isBefore(endTime);
+    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime start, LocalTime end) {
+        if (!start.isAfter(end)) {
+            return !lt.isBefore(start) && lt.isBefore(end);
+        }
+        return !lt.isBefore(start) || lt.isBefore(end);
     }
 }
